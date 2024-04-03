9781c0f218b4c0e36f5fe5ba302bf2b31046c247
Chris Burton <csburton@gmail.com>
2024-04-03 08:38:16 +0100

    Initial commit

---
+++ src/Exception/AbstractConfigException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Pantono\Config\Exception;
+
+abstract class AbstractConfigException extends \RuntimeException
+{
+}
---
+++ src/Exception/EnvironmentDoesNotExist.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Pantono\Config\Exception;
+
+class EnvironmentDoesNotExist extends AbstractConfigException
+{
+}
